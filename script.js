b40577cf2e79bb2ba5d3a1f178eb55f1f185361d
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-12 11:45:00 -0700

    created event listener to find data mealID when clicking image

--- script.js
+++ script.js
@@ -49,3 +49,16 @@ function searchMeal(e) {
 
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    console.log(mealID);
+  }
+});
